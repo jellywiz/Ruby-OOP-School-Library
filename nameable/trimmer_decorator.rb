530b5b520d42ee796f86ed3891409c6109924749
jellywiz <muzaffer.dler@gmail.com>
2023-02-06 18:58:05 +0300

    refactorize decoders

--- nameable/trimmer_decorator.rb
+++ nameable/trimmer_decorator.rb
@@ -2,6 +2,10 @@ require_relative 'nameable_decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable_obj.correct_name[0..9] if @nameable_obj.correct_name.length > 10
+    if @nameable.correct_name.length > 10
+      "#{@nameable.correct_name[0...9]}.."
+    else
+      @nameable.correct_name
+    end
   end
 end
